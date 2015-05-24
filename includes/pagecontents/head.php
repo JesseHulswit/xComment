f50709c9f22426423e27cd28a696deefa0d274b1
Jesse Hulswit <jhulswit@pirimid.net>
2015-05-24 20:25:23 +0200

    First Input

---
+++ includes/pagecontents/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<title>New Project</title>
+	
+	<link href="style/reset.css" rel="Stylesheet" type="text/css" />
+	<link href="style/style.css" rel="Stylesheet" type="text/css" />
+
+	
+</head>
---
+++ pages/home.php
@@ -0,0 +1,35 @@
+<?php
+/*include('includes/system/con.php');*/
+include('includes/pagecontents/head.php');
+
+?>
+
+		
+		
+<body>
+
+  
+<div id="frame">
+		
+		
+<?php include('includes/pagecontents/header.php');?>
+
+	
+
+
+
+
+
+
+
+<?include('includes/pagecontents/footer.php');?>
+
+
+
+</div> <!-- end of frame -->
+
+</body>
+
+
+
+</html>
